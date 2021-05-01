b44ae7c4960bbcdf1f59575bef75327d6a0bb781
VerschaerenJeroen <verschaerenjeroen@gmail.com>
2021-05-01 12:35:31 +0200

    Test

--- src/main/java/be/thomasmore/froomi/controllers/HomeController.java
+++ src/main/java/be/thomasmore/froomi/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controllers;
+package be.thomasmore.froomi.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/test/java/be/thomasmore/froomi/DemoApplicationTests.java
+++ src/test/java/be/thomasmore/froomi/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package be.thomasmore.froomi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
